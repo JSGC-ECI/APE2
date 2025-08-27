289ad908d29a83cfdb326abccbde2fbe54977482
Juan Sebastián Guayazán Clavijo <162055263+JSGC-ECI@users.noreply.github.com>
2025-08-27 09:43:10 -0500

    Update 3.r

--- 3.r
+++ 3.r
@@ -1,1 +1,21 @@
+x <- iris$Sepal.Length
+y <- iris$Sepal.Width
+
+xbar <- mean(x)
+ybar <- mean(y)
+
+n <- nrow(iris)
+
+cova <- (1/(n-1))* sum((x-xbar)*(y-ybar))
+cova
+cov(iris[,-5])
+
+varx <- sd(iris$Sepal.Length)
+vary <- sd(iris$Sepal.Width)
+
+corre <- cova/(varx * vary)
+corre
+
+corr <- cor(iris[,-5])
+corr
 
