df689ac23d1f12c161ad9f0f6144b033352f611d
Juan Sebastián Guayazán Clavijo <162055263+JSGC-ECI@users.noreply.github.com>
2025-08-29 09:28:04 -0500

    Update 5.r

--- Ejercicios Clase/Primer Tercio/5.r
+++ Ejercicios Clase/Primer Tercio/5.r
@@ -1,4 +1,5 @@
-n <- 10
+library(tidyverse)
+n <- 1000
 p <- 0.7
 x <- 0:n
 mu <- n*p
@@ -8,5 +9,5 @@ f.binom <- dbinom(x,n,p)
 f.norm <- dnorm(x, mu, sgm)
 df <- data_frame(x,f.binom, f.norm)
 ggplot(df, aes(x, f.binom, fill = x <= 6)) + geom_col() + geom_line(aes(x,f.norm))
-pbinom(6,n,p)
-pnorm(6,mu, sgm)
+pbinom(690,n,p)
+pnorm(690.5,mu,sgm)
