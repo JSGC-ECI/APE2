126f09fab88b37fc28aff41e13f03b2ff3f4cf8d
Juan Sebastián Guayazán Clavijo <162055263+JSGC-ECI@users.noreply.github.com>
2025-08-29 09:11:37 -0500

    Update 5.r

--- Ejercicios Clase/Primer Tercio/5.r
+++ Ejercicios Clase/Primer Tercio/5.r
@@ -1,1 +1,12 @@
-
+n <- 10
+p <- 0.7
+x <- 0:n
+mu <- n*p
+q <- 1-p
+sgm <- sqrt(n*p*q)
+f.binom <- dbinom(x,n,p)
+f.norm <- dnorm(x, mu, sgm)
+df <- data_frame(x,f.binom, f.norm)
+ggplot(df, aes(x, f.binom, fill = x <= 6)) + geom_col() + geom_line(aes(x,f.norm))
+pbinom(6,n,p)
+pnorm(6,mu, sgm)
